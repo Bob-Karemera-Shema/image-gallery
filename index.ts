5b2a4af5a69dbbed1da11e9fe35c15801342538f
Bob Karemera Shema <bobshema14@gmail.com>
2025-04-25 20:00:25 +0200

    Home page padding, navigation buttons on lightbox

--- index.ts
+++ index.ts
@@ -1,2 +1,11 @@
-const imageGrid = document.querySelector('.image-grid');
-const lightbox = document .querySelector('.lightbox');
+const imageGridEl = document.querySelector('.image-grid') as HTMLDivElement;
+const lightboxContainerEl = document.querySelector('.lightbox-container') as HTMLDivElement;
+const imageContainerEl = document.querySelector('.full-image-container') as HTMLDivElement;
+const authorEl = document.querySelector('.author') as HTMLSpanElement;
+const downloadEl = document.querySelector('.download-link')as HTMLAnchorElement;
+
+import { Image } from "./interfaces";
+
+const imageArray: Image[] = [];
+
+function fetchImages(): void {}
